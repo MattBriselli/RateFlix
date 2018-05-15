4e91c5294af5e1cf9f89adb3acc50df06bac7f96
Matt Briselli <briselli@adobe.com>
2018-05-15 07:32:52 -0400

    new project empty

---
+++ RateFlix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RateFlix
+//
+//  Created by Matt Briselli on 5/15/18.
+//  Copyright © 2018 brslli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
